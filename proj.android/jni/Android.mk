b0569f8238c1b668f4f91cd0ee1ba016e0ff45b7
pp <pp2@ppui-iMac.local>
2016-03-18 12:55:35 +0900

    add iap

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -1,32 +1,39 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(call \
+my-dir)
 
 include $(CLEAR_VARS)
 
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path, $(LOCAL_PATH))
 
 LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/json11.cpp \
-                   ../../Classes/connection.cpp \
-                   ../../Classes/user_info.cpp \
-                   ../../Classes/assets_scene.cpp \
-                   ../../Classes/lobby_scene.cpp \
-                   ../../Classes/single_play2_scene.cpp \
-                   ../../Classes/single_play_info.cpp \
-                   ../../Classes/resource_md.cpp \
-                   ../../Classes/multi_lobby_scene.cpp \
-                   ../../Classes/multi_room_scene.cpp \
-                   ../../Classes/multi_play_scene.cpp \
-                   ../../Classes/ranking_scene.cpp \
-                   ../../Classes/setting_scene.cpp \
-
+../../Classes/AppDelegate.cpp \
+../../Classes/json11.cpp \
+../../Classes/connection.cpp \
+../../Classes/user_info.cpp \
+../../Classes/assets_scene.cpp \
+../../Classes/lobby_scene.cpp \
+../../Classes/single_play2_scene.cpp \
+../../Classes/single_play_info.cpp \
+../../Classes/resource_md.cpp \
+../../Classes/multi_lobby_scene.cpp \
+../../Classes/multi_room_scene.cpp \
+../../Classes/multi_play_scene.cpp \
+../../Classes/ranking_scene.cpp \
+../../Classes/setting_scene.cpp
+LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
+LOCAL_LDLIBS := -landroid \
+-llog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_WHOLE_STATIC_LIBRARIES := PluginIAP \
+sdkbox \
+android_native_app_glue
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
@@ -40,6 +47,8 @@ LOCAL_STATIC_LIBRARIES := cocos2dx_static
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
+$(call import-module, ./sdkbox)
+$(call import-module, ./pluginiap)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
